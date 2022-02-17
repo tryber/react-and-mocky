f06ede90c15af9aacfc03dabf4914d605b25bc5e
Geovanna Carolline <geovanna@lenovo.lan>
2022-02-17 15:52:11 -0400

    Teste 1 e mock

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import responseAPI from './mocks';
 
 describe('Test Rick & Morty API', () => {
 
   beforeEach(()=>{
-    //Fazer o mock do fetch aqui 
+    global.fetch = jest.fn().mockResolvedValue( {
+      json: jest.fn().mockResolvedValue( responseAPI )
+    })
     
     render(<App/>)
   })
   
-  test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
-
+  test('Verifica se aparece o card com titulo de "Rick Sanchez"', async () => {
+    const titleEl = await screen.findByRole( 'heading', { name: 'Rick Sanchez' } );
+    expect( titleEl ).toBeInTheDocument();
   })
 
   test('Verifica se existem o input de texto e o botão "Buscar"', () => {
