d273e6c61f20e507d8514bb9a27136c0c9e0c92c
Marçal Mendonça <marcalmendrighi@gmail.com>
2022-06-03 17:23:20 -0300

    Execução dos exercícios

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { getByRole, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import responseAPI from './mocks';
 import App from '../App';
 
 describe('Test Rick & Morty API', () => {
 
   beforeEach(()=>{
-    //Fazer o mock do fetch aqui 
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(responseAPI)
+    })
     
     render(<App/>)
   })
   
   test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
-
+    const cardRick = screen.getByRole('img', {  name: /rick sanchez/i});
+    expect(cardRick).toBeInTheDocument();
   })
 
   test('Verifica se existem o input de texto e o botão "Buscar"', () => {
-    
+    const inputEl = screen.getByRole('textbox');
+    const buttonEl = screen.getByRole('button', {  name: /buscar/i});
+    expect(inputEl).toBeInTheDocument();
+    expect(buttonEl).toBeInTheDocument();
   })
 
   test('Verifica se ao buscar por "Smith" aparecem 4 cards', () => {
-    
+    const inputEl = screen.getByRole('textbox');
+    const buttonEl = screen.getByRole('button', {  name: /buscar/i});
+    userEvent.type(inputEl, 'Smith');
+    userEvent.click(buttonEl);
+    const cards = screen.getAllByRole('article')
+    expect(cards).toHaveLength(4);  
   })
 
 })
