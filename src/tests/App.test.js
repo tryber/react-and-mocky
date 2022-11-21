4c9cc8d15950f7d30de65ac734e1bd8bd3283178
Pablo Dominguez <pasaxo@gmail.com>
2022-11-21 09:02:35 -0300

    test 1,2,3

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,41 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import responseAPI from './mocks';
+
 
 describe('Test Rick & Morty API', () => {
 
   beforeEach(()=>{
-    //Fazer o mock do fetch aqui 
-    
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(responseAPI),
+    });
     render(<App/>)
   })
   
   test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
+    const renderRickSanches = screen.getByRole('heading', {  
+      name: /rick sanchez/i,
+      level: 3,
+  });
+
+    expect(renderRickSanches).toBeInTheDocument();
 
   })
 
   test('Verifica se existem o input de texto e o botão "Buscar"', () => {
+    const renderBottonBuscar = screen.getByRole('button', {  name: /buscar/i});
+    const renderInput = screen.getByRole('textbox');
     
+    expect(renderBottonBuscar).toBeInTheDocument();
+    expect(renderInput).toBeInTheDocument();
   })
 
   test('Verifica se ao buscar por "Smith" aparecem 4 cards', () => {
-    
+    userEvent.type(screen.getByRole('textbox'), 'smith');
+    userEvent.click(screen.getByRole('button', {  name: /buscar/i}));
+    const renderArticles = screen.queryAllByRole('article');
+    expect(renderArticles).toHaveLength(4);
   })
 
 })
