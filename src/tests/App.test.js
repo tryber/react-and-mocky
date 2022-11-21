4c6afb059d8b671c3bf3257117d682b59c306747
Jean Alves <j_alves_19@hotmail.com>
2022-11-20 20:56:20 -0400

    preenchendo os três primeiros testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Test Rick & Morty API', () => {
+  beforeEach(() => {
+    //Fazer o mock do fetch aqui
 
-  beforeEach(()=>{
-    //Fazer o mock do fetch aqui 
-    
-    render(<App/>)
-  })
-  
-  test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
+    render(<App />);
+  });
 
-  })
+  test('Verifica se aparece o card com titulo de `Rick Sanchez` numa tag `h3`', async () => {
+    await waitForElementToBeRemoved(() => screen.getByText('Carregando...'));
 
-  test('Verifica se existem o input de texto e o botão "Buscar"', () => {
-    
-  })
+    const name = screen.queryByRole('heading', { name: 'Rick Sanchez' });
 
-  test('Verifica se ao buscar por "Smith" aparecem 4 cards', () => {
-    
-  })
+    expect(name).toBeInTheDocument();
+  });
 
-})
+  test('Verifica se existem o input de texto e o botão com o texto `Buscar`', () => {
+    const input = screen.queryByRole('textbox');
+    const button = screen.queryByRole('button', { name: 'Buscar' });
+
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  test('Verifica se ao buscar por "Smith" aparecem 4 cards', async () => {
+    await waitForElementToBeRemoved(() => screen.getByText('Carregando...'));
+
+    const input = screen.queryByRole('textbox');
+    const button = screen.queryByRole('button', { name: 'Buscar' });
+
+    userEvent.type(input, 'Smith');
+    userEvent.click(button);
+
+    expect(await screen.findAllByRole('heading')).toHaveLength(4)
+  });
+});
